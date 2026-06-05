294f889b740cf446da5b9d0257e37f4f75584ad4
Kell Maglo <maglokell@gmail.com>
2026-06-05 18:09:09 -0400

    Storage needs rework

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,19 @@
 console.log("Testing, testing, 1 2 3")
 
+chrome.storage.session.get("www.google.com").then(
+    function(value){
+        console.log(value["www.google.com"])
+    }
+)
+
 const popupDiv = document.createElement("div")
 popupDiv.style = "width: 100px; " + 
     "height: 100px; " + 
     "background-color: white; " +
     "position: fixed; " +
     "top: 0px;"
+
+popupDiv.innerHTML = ""
 
 // Adds the popup
 // const mainBody = document.body
