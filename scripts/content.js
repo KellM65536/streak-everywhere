1de9376654861c2ac73eb84e7152b7b36a1fc7a5
Kell Maglo <maglokell@gmail.com>
2026-06-05 17:43:43 -0400

    Started work on popup

---
+++ scripts/content.js
@@ -0,0 +1,12 @@
+console.log("Testing, testing, 1 2 3")
+
+const popupDiv = document.createElement("div")
+popupDiv.style = "width: 100px; " + 
+    "height: 100px; " + 
+    "background-color: white; " +
+    "position: fixed; " +
+    "top: 0px;"
+
+// Adds the popup
+// const mainBody = document.body
+// mainBody.appendChild(popupDiv);
