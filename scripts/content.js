5ad45112a4fbb0a43a9e3d607bcbc9e4d2ba480d
Kell Maglo <maglokell@gmail.com>
2026-06-06 14:28:13 -0400

    Page visit now counts on page load, not on update

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,7 @@
+// Not synced with DAY_IN_MS in server-worker...
+const DAY_IN_MS = 1000 * 3//24 * 60 * 60 * 1000
+const today = Math.round(Date.now() / DAY_IN_MS)
+
 function animatePopIn(element){
     const SECOND_IN_FRAMES = 200
     let animationFrame = 0
@@ -136,13 +140,16 @@ function createStreakPopup(hostname, numVisits) {
 }
 
 // Request page visit counts from service worker
-chrome.runtime.sendMessage({action: "GET_DATA"}, (response) => {
-    const hostname = window.location.hostname
-    const numVisits = response.data[hostname].visits
+chrome.runtime.sendMessage({action: "GET_DATA", hostname: window.location.hostname}, (response) => {
+    console.log(response.data)
+    
+    if(window.location.hostname in response.data){
+        const hostname = window.location.hostname
+        const numVisits = response.data[hostname].visits
 
-    console.log(hostname)
-    console.log(numVisits)
+        console.log(hostname)
+        console.log(numVisits)
 
-    // Create elements for popup
-    createStreakPopup(hostname, numVisits)
+        createStreakPopup(hostname, numVisits)
+    }
 })
--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,29 +1,23 @@
-const DAY_IN_MS = 24 * 60 * 60 * 1000
+const DAY_IN_MS = 1000 * 3//24 * 60 * 60 * 1000
 
 // Send page visit counts to content script on request
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if(message.action == "GET_DATA"){
-    // console.log("Recieved signal")
-    chrome.storage.session.get().then((value) => {
-        sendResponse({data: value})
-      }
-    )
-  }
+    console.log("Recieved signal")
+    
+    // START BLOCK
 
-  return true
-})
+    const tabURL = message.hostname
+    // const tabLastAccess = tab.lastAccessed
 
-async function updateStreaks(tabId, changeInfo, tab){
-  const tabURL = new URL(tab.url).hostname
-  // const tabLastAccess = tab.lastAccessed
-
-  chrome.storage.session.get(tabURL).then(async (value) => {
-      newThingy = {}
+    chrome.storage.session.get(tabURL).then(async (value) => {
       const today = Math.round(Date.now() / DAY_IN_MS)
+
+      newThingy = {}
 
       if(value[tabURL]){
         // Site visited before
-        
+        console.log("visited site")
         if(today - value[tabURL].lastVisit >= 1) {
           // It's been a day since last visit
           
@@ -31,7 +25,7 @@ async function updateStreaks(tabId, changeInfo, tab){
             // Been 2 or more days, end streak
             newThingy[tabURL] = {
               visits: 1,
-              lastVisit: today
+              lastVisit: today,
             }
 
           } else {
@@ -39,7 +33,7 @@ async function updateStreaks(tabId, changeInfo, tab){
 
             newThingy[tabURL] = {
               visits: value[tabURL].visits + 1,
-              lastVisit: today
+              lastVisit: today,
             }
           }
 
@@ -47,26 +41,100 @@ async function updateStreaks(tabId, changeInfo, tab){
           await chrome.storage.session.set(
             newThingy
           )
+          
+          // console.log(newThingy)
+          // sendResponse({data: newThingy})
         }
       } else {
         // Completely new site
-
+        console.log("new site")
         newThingy[tabURL] = 
         {
           visits: 1,
-          lastVisit: today
+          lastVisit: today,
         }
 
-        // Update storage
+        // Update storage and send response
         await chrome.storage.session.set(
           newThingy
         )
+
+        // console.log(newThingy)
+        // sendResponse({data: newThingy})
+
       }
-    }
-  )
+      
+      sendResponse({data: newThingy})
+    })
 
-  // Printing for debugging
-  console.log(await chrome.storage.session.get())
-}
+    // Printing for debugging
+    // console.log(await chrome.storage.session.get())
 
-chrome.tabs.onUpdated.addListener(updateStreaks)
+    // END BLOCK
+
+    // chrome.storage.session.get().then((value) => {
+    //     sendResponse({data: value})
+    //   }
+    // )
+  }
+
+  return true
+})
+
+// async function updateStreaks(tabId, changeInfo, tab){
+//   const tabURL = new URL(tab.url).hostname
+//   // const tabLastAccess = tab.lastAccessed
+
+//   chrome.storage.session.get(tabURL).then(async (value) => {
+//       newThingy = {}
+//       const today = Math.round(Date.now() / DAY_IN_MS)
+
+//       if(value[tabURL]){
+//         // Site visited before
+        
+//         if(today - value[tabURL].lastVisit >= 1) {
+//           // It's been a day since last visit
+          
+//           if(today - value[tabURL].lastVisit >= 2){
+//             // Been 2 or more days, end streak
+//             newThingy[tabURL] = {
+//               visits: 1,
+//               lastVisit: today,
+//             }
+
+//           } else {
+//             // Less than 2 days, streak is find
+
+//             newThingy[tabURL] = {
+//               visits: value[tabURL].visits + 1,
+//               lastVisit: today,
+//             }
+//           }
+
+//           // Update storage
+//           await chrome.storage.session.set(
+//             newThingy
+//           )
+//         }
+//       } else {
+//         // Completely new site
+
+//         newThingy[tabURL] = 
+//         {
+//           visits: 1,
+//           lastVisit: today,
+//         }
+
+//         // Update storage
+//         await chrome.storage.session.set(
+//           newThingy
+//         )
+//       }
+//     }
+//   )
+
+//   // Printing for debugging
+//   console.log(await chrome.storage.session.get())
+// }
+
+// chrome.tabs.onUpdated.addListener(updateStreaks)
