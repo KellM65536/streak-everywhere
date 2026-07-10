adc3b3c811bf3e4214a7853cf171c6c84d18cce2
Kell Maglo <maglokell@gmail.com>
2026-07-10 18:46:00 -0400

    Popup has rough table of streaks

--- popup.js
+++ popup.js
@@ -6,6 +6,18 @@ console.log("i ran!")
 chrome.runtime.sendMessage({action: "GET_DATABASE"}, (response) => {
     console.log("I got a response! (I think)")
     console.log(response.data)
+
+    for (const [key, value] of Object.entries(response.data)){
+        const newRow = document.createElement("tr")
+        const newURL = document.createElement("td")
+        newURL.textContent = key
+        const newVisists = document.createElement("td")
+        newVisists.textContent = value.visits
+        
+        newRow.appendChild(newURL)
+        newRow.appendChild(newVisists)
+        streakTableBody.appendChild(newRow)
+    }
     
     // if(window.location.hostname in response.data){
     //     const hostname = window.location.hostname
