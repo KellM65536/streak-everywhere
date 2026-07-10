40c6e6b79a5fb43480738c50fd2e2b9acd1d3175
Kell Maglo <maglokell@gmail.com>
2026-07-10 19:53:53 -0400

    Added show all button

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,15 @@
+const DEFAULT_NUM_ROWS = 10
 let streakTableBody = document.getElementById("streak-table")
+let showAllButton = document.getElementById("show-all-button")
+let allShown = false
+let responseData
 
 function entryMore(a, b) {
     return b[1].visits - a[1].visits
 }
 
-// Request all page visit counts from service worker
-chrome.runtime.sendMessage({action: "GET_DATABASE"}, (response) => {
-    for (const [key, value] of Object.entries(response.data).sort(entryMore).slice(0, 10)){
+function displayResponseAsTable(numRows){
+    for (const [key, value] of Object.entries(responseData).sort(entryMore).slice(0, numRows)){
         // Create and add table rows
         const newRow = document.createElement("tr")
         const newURL = document.createElement("td")
@@ -17,5 +20,25 @@ chrome.runtime.sendMessage({action: "GET_DATABASE"}, (response) => {
         newRow.appendChild(newURL)
         newRow.appendChild(newVisists)
         streakTableBody.appendChild(newRow)
+    }
+}
+
+// Request all page visit counts from service worker
+chrome.runtime.sendMessage({action: "GET_DATABASE"}, (response) => {
+    responseData = response.data
+    displayResponseAsTable(DEFAULT_NUM_ROWS)
+})
+
+showAllButton.addEventListener("click", () => {
+    streakTableBody.replaceChildren()
+
+    if(allShown){
+        displayResponseAsTable(DEFAULT_NUM_ROWS)
+        showAllButton.textContent = "Show All"
+        allShown = false
+    } else {
+        displayResponseAsTable()
+        showAllButton.textContent = "Show Less"
+        allShown = true
     }
 })
